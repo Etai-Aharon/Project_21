a3c7edff86353b68cca1cbf298ee3db161a89be5
Etai <etaidude@gmail.com>
2021-03-26 01:33:56 +0200

    V2 - simple server. return self

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 # first version test - push to github
 
-print('file pushed to github')
+import http.server as SimpleHTTPServer
+import socketserver as SocketServer
+import logging
+
+PORT = 8000
+
+class GetHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+
+    def do_GET(self):
+        # print('got client')
+        # logging.error(self.headers)
+        SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+        # self.send_response(200, "")
+
+
+Handler = GetHandler
+httpd = SocketServer.TCPServer(("", PORT), Handler)
+
+httpd.serve_forever()
