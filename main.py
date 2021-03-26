c1865fa624fb094e7a4b45661909429ff684ee86
Etai <etaidude@gmail.com>
2021-03-26 11:33:34 +0300

    V2 - simple server. return 200 with text lines

--- main.py
+++ main.py
@@ -2,17 +2,19 @@
 
 import http.server as SimpleHTTPServer
 import socketserver as SocketServer
-import logging
 
 PORT = 8000
+
 
 class GetHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 
     def do_GET(self):
-        # print('got client')
-        # logging.error(self.headers)
-        SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
-        # self.send_response(200, "")
+        self.send_response(200)
+        response = b'Response from server!\nPage text line 2\nPage text line 3'
+        self.send_header('Content-type', 'text/unknown')
+        # self.send_header('Content-Length', len(response))
+        self.end_headers()
+        self.wfile.write(response)
 
 
 Handler = GetHandler
